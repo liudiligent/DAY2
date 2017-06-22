a68dd8999978d14d648ea916c40dc3c6526301ac
liudiligent <806970263@qq.com>
2017-06-22 22:31:32 +0800

    Create HELLOWORLD.js

---
+++ HELLOWORLD.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title></title>
+		<script>
+			 
+			 
+			 var arr1=9;
+			 var arr2=4;
+			 var arr3=3;
+			 function Mod(arr1,arr2){
+			 
+			    var arr;
+			    arr=arr1%arr2;
+			 	return arr;
+			 }
+			
+			var v=Mod(arr1,arr2);
+			var v1=Mod(arr1,arr3);
+			console.log(v);
+			console.log(v1);
+		</script>
+	</head>
+	<body>
+	</body>
+</html>
